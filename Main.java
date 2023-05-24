105931cf27b834ab9b419c8639bd4ce4648513c6
kekeldaquebrada <124931192+kekeldaquebrada@users.noreply.github.com>
2023-05-24 15:50:08 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Olá Mundo!");
+    int num = 5;
+    System.out.println(num);
+    int myNum = -2;
+    float myFloatNum = 7.34f;
+    char myLetter = 'R';
+    boolean myBool = false;
+    String myText = "Olá como é legal ser eu";
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+    //parte dois do codigo
+    System.out.println(myText + myBool);
+    int x = 5;
+    float y = x;
+    System.out.println(x);
+    System.out.println(y);
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble;
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }
+}
