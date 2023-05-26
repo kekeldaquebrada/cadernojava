7a3e793aaf07441e74c0142113a12a5640bc1f15
kekeldaquebrada <124931192+kekeldaquebrada@users.noreply.github.com>
2023-05-26 15:36:19 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,20 @@
+fun main() {
+  println("Olá mundo!")
+  var nome: String = "Raquel"
+  val nascimento: Int = 2005
+
+  println(nome)          // exibe o nome
+  println(nascimento)     // exime o nascimento
+
+  val myNum = 5             // Int
+  val myDoubleNum = 5.99    // Double
+  val myLetter = 'D'        // Char
+  val myBoolean = true      // Boolean
+  val myText = "Olá"      // String
+
+  println (myNum)
+  println (myDoubleNum)
+  println (myLetter)
+  println (myBoolean)
+  println (myText)
+}
