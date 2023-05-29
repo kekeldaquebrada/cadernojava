ddc2c6461780a46e17cf6641ef4e79cb4d49e4c0
kekeldaquebrada <124931192+kekeldaquebrada@users.noreply.github.com>
2023-05-29 16:19:49 -0300

    Add files via upload

---
+++ main (2).kt
@@ -0,0 +1,22 @@
+fun main() {
+  var x = 13
+  var y = 3
+  var soma=x+y
+  println("A soma é:"+soma)
+  var divisao=x/y
+  println("A divisao é:"+divisao)
+  var sub=x-y
+  println("A subtraçao é:"+sub)
+  var multi=x*y
+  println("A multiplicaçao é:"+multi)
+  //
+  var rest=x%y
+  println("O resto da divisao é:"+rest)
+  println("O x é:"+ x)
+  //
+  ++x
+  println("Agora o x é:" +x)
+  --x
+  println("Agora o x menos 1 é:"+x)
+  
+}
